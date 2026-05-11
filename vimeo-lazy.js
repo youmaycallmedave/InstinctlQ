d2e0da8a73dc804c8e6432dc95de898ab3d1e13f
David Zimenkov <158498508+youmaycallmedave@users.noreply.github.com>
2026-05-11 15:14:57 +0200

    Static + Bg Vimeo Lazy

---
+++ vimeo-lazy.js
@@ -0,0 +1,43 @@
+document.addEventListener('click', function (e) {
+    var wrapper = e.target.closest('.vimeo_lazy-wrap');
+    if (!wrapper || wrapper.classList.contains('vimeo_lazy-bg') || wrapper.querySelector('iframe')) return;
+
+    var videoId = wrapper.getAttribute('data-vimeo-id');
+
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute('src', 'https://player.vimeo.com/video/' + videoId + '?autoplay=1&dnt=1');
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('allow', 'autoplay; fullscreen; picture-in-picture');
+    iframe.setAttribute('allowfullscreen', '');
+
+    wrapper.innerHTML = '';
+    wrapper.appendChild(iframe);
+});
+
+function loadBgVideo(el) {
+    if (el.querySelector('iframe')) return;
+
+    var videoId = el.getAttribute('data-vimeo-id');
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute('src', 'https://player.vimeo.com/video/' + videoId + '?background=1&dnt=1');
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('allow', 'autoplay');
+
+    el.appendChild(iframe);
+
+    iframe.addEventListener('load', function () {
+        var poster = el.querySelector('.vimeo_lazy-poster');
+        if (poster) poster.style.opacity = '0';
+    });
+}
+
+document.querySelectorAll('.vimeo_lazy-bg').forEach(function (el) {
+    ScrollTrigger.create({
+        trigger: el,
+        start: 'top bottom+=200',
+        once: true,
+        onEnter: function () {
+            loadBgVideo(el);
+        }
+    });
+});
