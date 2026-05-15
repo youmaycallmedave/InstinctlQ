ad316265489a8f17d0a752d334c37e69f51dc340
David Zimenkov <158498508+youmaycallmedave@users.noreply.github.com>
2026-05-15 16:27:49 +0200

    Добавил обратный схлопывание раньше

--- menu/script.js
+++ menu/script.js
@@ -1,5 +1,10 @@
+
 ScrollTrigger.create({
   start: "100px top",
   onEnter: () => document.querySelector(".navbar1_component").classList.add("is-active"),
+});
+
+ScrollTrigger.create({
+  start: "400px top",
   onLeaveBack: () => document.querySelector(".navbar1_component").classList.remove("is-active"),
 });
