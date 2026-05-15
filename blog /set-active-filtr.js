151b8823134f65d6822c299dc45fe4d8f0f9398c
David Zimenkov <158498508+youmaycallmedave@users.noreply.github.com>
2026-05-15 12:50:33 +0200

    init

---
+++ blog /set-active-filtr.js
@@ -0,0 +1,4 @@
+  window.addEventListener('load', function () {
+    var link = document.querySelector('.blog-filter_list *:first-child .category-filter-link');
+    if (link) link.classList.add('is-active');
+  });
